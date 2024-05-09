87efb25c77a5d166c36d2493c3dbbfcd121c64ed
xuand <xuandangvinh@gmail.com>
2024-05-09 20:36:24 +0700

    Add to BaseTest in Project - First commiet

---
+++ BaseTest/baseTest.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Login_POM.BaseTest;
+
+public class baseTest
+{
+    protected IWebDriver _driver;
+    [SetUp]
+    public void setup()
+    {
+        _driver = new ChromeDriver();
+        _driver.Navigate().GoToUrl("https://www.facebook.com/login/");
+        _driver.Manage().Window.Maximize();
+    }
+    [TearDown]
+    public void close()
+    {
+        _driver.Dispose();
+    }
+}
