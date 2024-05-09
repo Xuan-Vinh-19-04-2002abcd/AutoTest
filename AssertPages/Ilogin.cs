1fef4b3b260c27292050c99c0c2581e9af24d18e
xuand <xuandangvinh@gmail.com>
2024-05-09 20:40:24 +0700

    fsdf

---
+++ AssertPages/Ilogin.cs
@@ -0,0 +1,9 @@
+﻿namespace Login_POM.AssertPages;
+
+public interface Ilogin
+{
+    void loginSuccessfullyWithValidData(string expertResult, string actualResult);
+    void loginSuccessfullyWithInvalidData(string expertResult, string actualResult);
+    void loginSuccessfullyWithEmailBlank(string expertResult, string actualResult);
+    void loginSuccessfullyWithPasswordBlank(string expertResult, string actualResult);
+}
